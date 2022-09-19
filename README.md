607627fb016c11208f3856af8e49b83c2b717231
Lewis Oaten <lewis@razorsecure.com>
2022-09-19 19:34:30 +0100

    First commit! Created base app connecting through from a React frontend, Rocket API, and SQLx DB.

---
+++ README.md
@@ -0,0 +1,10 @@
+# caLANdar
+
+Web application for helping organising LAN parties.
+
+## Development
+
+Launch local development environment with:
+`docker-compose up`
+
+Rust API and frontend will both live-reload following changes.
---
+++ api/src/main.rs
@@ -0,0 +1,38 @@
+#[macro_use] extern crate rocket;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::postgres::PgPool;
+
+#[get("/")]
+async fn index(pool: &rocket::State<PgPool>) -> String {
+    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
+    let test_id: i32 = match sqlx::query!("SELECT id FROM test")
+        .fetch_one(pool.inner())
+        .await {
+            Ok(test_id) => test_id.id,
+            Err(e) => {
+                // One day we should handle errors better (HTTP return codes)
+                println!("Error: {}", e);
+                return "Error".to_string();
+            }
+        };
+
+    return format!("Hello {test_id}!");
+}
+
+#[launch]
+async fn rocket() -> _ {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:password@db/calandar")
+        .await
+        .expect("Failed to create pool.");
+
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .expect("Failed to migrate database.");
+
+    rocket::build()
+        .manage::<PgPool>(pool)
+        .mount("/api", routes![index])
+}
