fb3e99a8350b5c33c68a3b1e321e11aa5adbe451
Lewis Oaten <lewis@razorsecure.com>
2022-10-22 17:29:07 +0100

    Make events table work better on mobile devices.

--- frontend/src/components/EventTable.tsx
+++ frontend/src/components/EventTable.tsx
@@ -1,18 +1,137 @@
 import * as React from "react";
 import { useEffect, useState, useContext } from "react";
+import { Box, Typography, Paper, Popper } from "@mui/material";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Button,
-  Typography,
-} from "@mui/material";
-import { Link } from "react-router-dom";
+  DataGrid,
+  GridColumns,
+  GridRowParams,
+  GridActionsCellItem,
+  GridValueFormatterParams,
+  GridValueGetterParams,
+  GridRenderCellParams,
+  GridRowId,
+} from "@mui/x-data-grid";
+import LaunchIcon from "@mui/icons-material/Launch";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../UserProvider";
 import { dateParser } from "../utils";
 import { EventData } from "../types/events";
+
+interface GridCellExpandProps {
+  value: string;
+  width: number;
+}
+
+function isOverflown(element: Element): boolean {
+  return (
+    element.scrollHeight > element.clientHeight ||
+    element.scrollWidth > element.clientWidth
+  );
+}
+
+const GridCellExpand = React.memo(function GridCellExpand(
+  props: GridCellExpandProps,
+) {
+  const { width, value } = props;
+  const wrapper = React.useRef<HTMLDivElement | null>(null);
+  const cellDiv = React.useRef(null);
+  const cellValue = React.useRef(null);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [showFullCell, setShowFullCell] = React.useState(false);
+  const [showPopper, setShowPopper] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    const isCurrentlyOverflown = isOverflown(cellValue.current!);
+    setShowPopper(isCurrentlyOverflown);
+    setAnchorEl(cellDiv.current);
+    setShowFullCell(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowFullCell(false);
+  };
+
+  React.useEffect(() => {
+    if (!showFullCell) {
+      return undefined;
+    }
+
+    function handleKeyDown(nativeEvent: KeyboardEvent) {
+      // IE11, Edge (prior to using Bink?) use 'Esc'
+      if (nativeEvent.key === "Escape" || nativeEvent.key === "Esc") {
+        setShowFullCell(false);
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [setShowFullCell, showFullCell]);
+
+  return (
+    <Box
+      ref={wrapper}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      sx={{
+        alignItems: "center",
+        lineHeight: "24px",
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        display: "flex",
+      }}
+    >
+      <Box
+        ref={cellDiv}
+        sx={{
+          height: "100%",
+          width,
+          display: "block",
+          position: "absolute",
+          top: 0,
+        }}
+      />
+      <Box
+        ref={cellValue}
+        sx={{
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+        }}
+      >
+        {value}
+      </Box>
+      {showPopper && (
+        <Popper
+          open={showFullCell && anchorEl !== null}
+          anchorEl={anchorEl}
+          style={{ width, marginLeft: -17 }}
+        >
+          <Paper
+            elevation={1}
+            style={{ minHeight: wrapper.current!.offsetHeight - 3 }}
+          >
+            <Typography variant="body2" style={{ padding: 8 }}>
+              {value}
+            </Typography>
+          </Paper>
+        </Popper>
+      )}
+    </Box>
+  );
+});
+
+function renderCellExpand(params: GridRenderCellParams<string>) {
+  return (
+    <GridCellExpand
+      value={params.value || ""}
+      width={params.colDef.computedWidth}
+    />
+  );
+}
 
 interface EventTableProps {
   eventsState?: [
@@ -30,6 +149,86 @@ export default function EventTable(props: EventTableProps) {
 
   const [events, setEvents] = props.eventsState ?? ownEventsState;
   const isAdmin = props.asAdmin ?? false;
+
+  const navigate = useNavigate();
+
+  const openEvent = React.useCallback(
+    (id: GridRowId) => () => {
+      navigate(`${id}`);
+    },
+    [],
+  );
+
+  const columns = React.useMemo<GridColumns<typeof events[number]>>(
+    () => [
+      {
+        field: "title",
+        headerName: "Title",
+        type: "string",
+        flex: 1,
+        editable: false,
+        renderCell: renderCellExpand,
+      },
+      {
+        field: "timeBegin",
+        headerName: "Start",
+        type: "dateTime",
+        flex: 1,
+        editable: false,
+        valueFormatter: (params: GridValueFormatterParams<moment.Moment>) => {
+          if (params.value == null) {
+            return "";
+          }
+
+          return params.value.calendar();
+        },
+      },
+      {
+        field: "duration",
+        headerName: "Duration",
+        type: "dateTime",
+        flex: 1,
+        editable: false,
+        valueGetter: (params: GridValueGetterParams) => {
+          if (params.row.timeBegin == null || params.row.timeEnd == null) {
+            return "";
+          }
+
+          return params.row.timeEnd
+            .endOf("day")
+            .diff(params.row.timeBegin.startOf("day"), "days");
+        },
+        valueFormatter: (params: GridValueFormatterParams<number>) => {
+          if (params.value == null) {
+            return "";
+          }
+
+          return `${params.value} days`;
+        },
+      },
+      {
+        field: "description",
+        headerName: "Description",
+        type: "string",
+        flex: 2,
+        editable: false,
+        renderCell: renderCellExpand,
+      },
+      {
+        field: "actions",
+        type: "actions",
+        flex: 0.1,
+        getActions: (params: GridRowParams) => [
+          <GridActionsCellItem
+            icon={<LaunchIcon />}
+            label="Open"
+            onClick={openEvent(params.id)}
+          />,
+        ],
+      },
+    ],
+    [openEvent],
+  );
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_PROXY}/api/events?as_admin=${isAdmin}`, {
@@ -55,36 +254,22 @@ export default function EventTable(props: EventTableProps) {
       <Typography component="h2" variant="h6" color="primary" gutterBottom>
         Upcoming Events
       </Typography>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Title</TableCell>
-            <TableCell>Start Date</TableCell>
-            <TableCell>End Date</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {events.map((events) => (
-            <TableRow key={events.id}>
-              <TableCell>{events.title}</TableCell>
-              <TableCell>{events.timeBegin.calendar()}</TableCell>
-              <TableCell>{events.timeEnd.calendar()}</TableCell>
-              <TableCell>{events.description}</TableCell>
-              <TableCell>
-                <Button
-                  variant="text"
-                  component={Link}
-                  to={events.id.toString()}
-                >
-                  Open
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <Box sx={{ width: "100%" }}>
+        <DataGrid
+          rows={events}
+          columns={columns}
+          pageSize={5}
+          autoHeight
+          rowsPerPageOptions={[5]}
+          disableSelectionOnClick
+          experimentalFeatures={{ newEditingApi: true }}
+          initialState={{
+            sorting: {
+              sortModel: [{ field: "timeBegin", sort: "desc" }],
+            },
+          }}
+        />
+      </Box>
     </React.Fragment>
   );
 }
