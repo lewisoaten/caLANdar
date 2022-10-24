57807c592f9b088586c3b9c826dadebe53983be4
Lewis Oaten <lewis@razorsecure.com>
2022-10-24 19:34:18 +0100

    Added UI aspects

--- frontend/src/components/Event.tsx
+++ frontend/src/components/Event.tsx
@@ -1,11 +1,19 @@
 import * as React from "react";
 import { useEffect, useState, useContext } from "react";
-import { Container, Paper, Grid, Typography } from "@mui/material";
+import {
+  Container,
+  Paper,
+  Grid,
+  Typography,
+  Skeleton,
+  Avatar,
+} from "@mui/material";
 import { UserContext } from "../UserProvider";
 import { useParams } from "react-router-dom";
 import { dateParser } from "../utils";
 import { EventData, defaultEventData } from "../types/events";
 import InvitationResponse from "./InvitationResponse";
+import EventGameSuggestions from "./EventGameSuggestions";
 
 const Event = () => {
   const userDetails = useContext(UserContext);
@@ -44,14 +52,13 @@ const Event = () => {
               p: 2,
               display: "flex",
               flexDirection: "column",
-              height: 240,
             }}
           >
             <Grid container spacing={2}>
               <Grid item xs={6}>
                 <Typography
                   component="h2"
-                  variant="h6"
+                  variant="h4"
                   color="primary"
                   gutterBottom
                 >
@@ -85,6 +92,68 @@ const Event = () => {
                 {loaded && <InvitationResponse event_id={event.id} />}
               </Grid>
             </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} md={6} lg={6}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography
+                  component="h3"
+                  variant="h6"
+                  color="primary"
+                  gutterBottom
+                >
+                  Attendee List
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  sx={{ whiteSpace: "pre-wrap" }}
+                >
+                  Coming Soon...
+                </Typography>
+              </Grid>
+              {Array.apply(null, Array(5)).map((invitation) => (
+                <React.Fragment>
+                  <Grid item xs={2}>
+                    <Skeleton variant="circular" animation="wave">
+                      <Avatar />
+                    </Skeleton>
+                  </Grid>
+                  <Grid item xs={10}>
+                    <Skeleton
+                      variant="rectangular"
+                      width="100%"
+                      animation="wave"
+                    >
+                      <Typography variant="body1" gutterBottom>
+                        Person Name
+                      </Typography>
+                    </Skeleton>
+                  </Grid>
+                </React.Fragment>
+              ))}
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} md={6} lg={6}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            {loaded && <EventGameSuggestions event_id={event.id} />}
           </Paper>
         </Grid>
       </Grid>
---
+++ frontend/src/components/EventGameSuggestions.tsx
@@ -0,0 +1,249 @@
+import * as React from "react";
+import { useEffect, useState, useContext, useRef } from "react";
+import {
+  Autocomplete,
+  Typography,
+  Grid,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  TextField,
+  CircularProgress,
+  AutocompleteChangeReason,
+  AutocompleteChangeDetails,
+  AutocompleteHighlightChangeReason,
+  IconButton,
+} from "@mui/material";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LaunchIcon from "@mui/icons-material/Launch";
+import { UserContext } from "../UserProvider";
+import { dateParser } from "../utils";
+import {
+  GameSuggestion,
+  defaultGameSuggestions,
+  Game,
+  defaultGames,
+} from "../types/game_suggestions";
+
+interface EventTableProps {
+  event_id: number;
+}
+
+export default function EventGameSuggestions(props: EventTableProps) {
+  const userDetails = useContext(UserContext);
+  const token = userDetails?.token;
+
+  const [gameSuggestions, setGameSuggestions] = useState(
+    defaultGameSuggestions,
+  );
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [options, setOptions] = useState(defaultGames);
+
+  var typingTimer = useRef<NodeJS.Timeout>();
+  const doneTypingInterval = 1000;
+
+  useEffect(() => {
+    fetch(
+      `${process.env.REACT_APP_API_PROXY}/api/events/${props.event_id}/games`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      },
+    )
+      .then((response) => {
+        return response
+          .text()
+          .then(
+            (data) => JSON.parse(data, dateParser) as Array<GameSuggestion>,
+          );
+      })
+      .then((data) => {
+        sortAndAddGameSuggestions(data);
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function sortAndAddGameSuggestions(
+    newGameSuggestions: Array<GameSuggestion>,
+  ) {
+    newGameSuggestions.sort((a, b) => a.name.localeCompare(b.name));
+    setGameSuggestions(newGameSuggestions);
+  }
+
+  const handleInputChange = (
+    event: React.SyntheticEvent,
+    value: string,
+    reason: string,
+  ) => {
+    // Prevent page reload
+    event.preventDefault();
+
+    clearTimeout(typingTimer.current);
+
+    setInputValue(value);
+    setErrorMessage("");
+
+    if (reason === "input") {
+      setLoading(true);
+      setOpen(true);
+
+      typingTimer.current = setTimeout(function () {
+        fetch(
+          `${process.env.REACT_APP_API_PROXY}/api/steam-game?query=${value}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + token,
+            },
+          },
+        )
+          .then((response) => {
+            return response
+              .text()
+              .then((data) => JSON.parse(data, dateParser) as Array<Game>);
+          })
+          .then((data) => {
+            setLoading(false);
+            if (!data || data.length === 0) {
+              setErrorMessage("No games found");
+              setOptions(defaultGames);
+              setOpen(false);
+            } else {
+              setOptions(data);
+            }
+          });
+      }, doneTypingInterval);
+    } else {
+      setOpen(false);
+      setOptions(defaultGames);
+    }
+  };
+
+  const handleInputSelect = (
+    event: React.SyntheticEvent<Element, Event>,
+    value: Game | null,
+    reason: AutocompleteChangeReason,
+    details?: AutocompleteChangeDetails<Game> | undefined,
+  ) => {
+    // Prevent page reload
+    event.preventDefault();
+    setOpen(false);
+
+    if (reason === "selectOption" && value) {
+      fetch(
+        `${process.env.REACT_APP_API_PROXY}/api/events/${props.event_id}/games`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+          body: JSON.stringify({
+            appid: value.appid,
+            name: value.name,
+          }),
+        },
+      )
+        .then((response) => {
+          setInputValue("");
+          return response
+            .text()
+            .then((data) => JSON.parse(data, dateParser) as GameSuggestion);
+        })
+        .then((data) => {
+          sortAndAddGameSuggestions([...gameSuggestions, data]);
+        });
+    }
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography component="h3" variant="h6" color="primary" gutterBottom>
+          Game Suggestions
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Autocomplete
+          id="game-suggestion"
+          open={open}
+          onOpen={() => {
+            setOpen(true);
+          }}
+          onClose={() => {
+            setOpen(false);
+          }}
+          isOptionEqualToValue={(option, value) => option.name === value.name}
+          getOptionLabel={(option) => option.name}
+          options={options}
+          getOptionDisabled={(option) =>
+            gameSuggestions.find((x) => x.appid === option.appid) ? true : false
+          }
+          handleHomeEndKeys={false}
+          loading={loading}
+          value={null}
+          inputValue={inputValue}
+          openOnFocus={false}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Suggest Game"
+              error={errorMessage ? true : false}
+              helperText={errorMessage}
+              InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <React.Fragment>
+                    {loading ? (
+                      <CircularProgress color="inherit" size={20} />
+                    ) : null}
+                    {params.InputProps.endAdornment}
+                  </React.Fragment>
+                ),
+              }}
+            />
+          )}
+          filterOptions={(x) => x}
+          onInputChange={handleInputChange}
+          onChange={handleInputSelect}
+        />
+        <List>
+          {gameSuggestions.map((gameSuggestion) => (
+            <ListItem
+              secondaryAction={
+                <IconButton
+                  aria-label="steam link"
+                  href={`https://store.steampowered.com/app/${gameSuggestion.appid}/`}
+                  target="_blank"
+                >
+                  <LaunchIcon />
+                </IconButton>
+              }
+              dense={true}
+            >
+              <ListItemAvatar>
+                <Avatar>
+                  <SportsEsportsIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={gameSuggestion.name}
+                secondary={`Suggested: ${gameSuggestion.requested_at.format(
+                  "ll",
+                )}`}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
+    </Grid>
+  );
+}
