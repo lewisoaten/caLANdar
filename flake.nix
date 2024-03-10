19e62157a0a402aeaed2567c698473da4c3c5ad4
Lewis Oaten <lewis@oaten.name>
2024-03-10 13:29:57 +0000

    Added chunking to game inserts so we don't overload the pool (#694)

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
             pkg-config
             postgresql
             pre-commit
+            nodejs_21
           ];
 
           # buildInputs: Should be used for things that need to be linked against (e.g. openssl). These will be of the hostPlaform's architecture. With strictDeps = true; (or by extension cross-platform builds), these will not be added to PATH. However, linking related variables will capture these packages (e.g. NIX_LD_FLAGS, CMAKE_PREFIX_PATH, PKG_CONFIG_PATH)
