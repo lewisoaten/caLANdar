9dd1d3dcfa5d85201c86f7db926f60a3ec7800a5
lewisoaten <lewis@oaten.name>
2024-10-18 21:57:50 +0100

    Fix for invalid index when there is existing data

--- api/migrations/20241018200316_email-lowercase-index.down.sql
+++ api/migrations/20241018200316_email-lowercase-index.down.sql
@@ -1,6 +1,5 @@
 -- Add down migration script here
 DROP INDEX IF EXISTS invitation_lower_email;
 DROP INDEX IF EXISTS event_game_vote_lower_email;
-DROP INDEX IF EXISTS event_game_lower_email;
 DROP INDEX IF EXISTS profiles_lower_email;
 DROP INDEX IF EXISTS user_game_lower_email;
